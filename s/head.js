600ca77511b71601e9154facb8e2b1b0c8919e93
bradleysansom <bjsansom@outlook.com>
2020-05-05 19:06:10 +0100

    src

---
+++ s/head.js
@@ -0,0 +1,28 @@
+var endpoint = "https://jsonbox.io/box_f4a3d05ef8fe374ac8ea";
+
+function fetchJSON(a) {
+    var f = new XMLHttpRequest;
+    f.open("GET", a, false);
+    f.send(null);
+    return f.responseText
+}
+
+function isURL(a) {
+    let url = a
+    if (!a.startsWith("javascript:")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+var hashh = window.location.hash.substr(1);
+if (window.location.hash != "") {
+    var res = JSON.parse(fetchJSON(endpoint + "/?q=s:" + hashh))[0];
+    var data = res["l"];
+    console.log(data);
+    if (data != null) {
+        if (isURL(data)) {
+            window.location.href = data;
+        }
+    }
+}
