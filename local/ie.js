7ad50d92239289053d3ca583321a0869a0730492
bradleysansom <bjsansom@outlook.com>
2020-06-30 16:08:23 +0100

    Update ie.js

--- local/ie.js
+++ local/ie.js
@@ -1,3 +1,3 @@
 if (window.document.documentMode) {
-    alert('Sorry!\nThis website does not work in Internet Explorer.\nPlease use another browser, such as Google Chrome or Firefox, to enjoy My Local Area.\pInstalling a new browser will also allow you to use new features on other websites.\n\nOnce again, My Local Area apologises for any inconvenience caused.')
+    alert('Sorry!\nThis website does not work in Internet Explorer.\nPlease use another browser, such as Google Chrome or Firefox, to enjoy My Local Area.\nInstalling a new browser will also allow you to use new features on other websites.\n\nOnce again, My Local Area apologises for any inconvenience caused.')
 }
