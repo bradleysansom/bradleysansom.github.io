64299e2b9b7a1f8ce03cd048e4b480d44c79caca
bradleysansom <bjsansom@outlook.com>
2020-07-09 01:37:28 +0100

    finalmente

--- local/darklight.js
+++ local/darklight.js
@@ -1,4 +1,6 @@
-const darklight = document.querySelector('.toggleDarkTheme');
-darklight.addEventListener('click', function() {
-    document.body.classList.toggle('darkTheme');
-})
+window.onload = function() {
+    const darklight = document.querySelector('.toggleDarkTheme');
+    darklight.addEventListener('click', function() {
+        document.body.classList.toggle('darkTheme');
+    })
+}
