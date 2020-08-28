b61ef3168e7d3fd2deb8de0771f7e79a6ea56658
bradleysansom <bjsansom@outlook.com>
2020-08-28 01:33:54 +0100

    local again

--- local/postcodes.js
+++ local/postcodes.js
@@ -1,6 +1,21 @@
-userPostcode = input;
+function postcodeValidation(p) {
+    var postcodeRegEx = /^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$/i;
+    return postcodeRegEx.test(p);
+}
+
+
+var userPostcode = document.getElementById("userPostcode").value;
 fetch('https://api.postcodes.io/postcodes/' + userPostcode)
-    .then(response => response.json())
+    .then(response => {
+        if (response.ok) {
+            return response.json()
+        } else {
+            return Promise.reject({
+                status: response.status,
+                statusText: response.statusText
+            })
+        }
+    })
     .then(data => {
         // Here's a list of repos!
         console.log(data)
