5b983015bef817f3e97a20531ed6e899a1dcfd7c
bradleysansom <bjsansom@outlook.com>
2020-08-29 01:07:23 +0100

    images

--- local/darklight.js
+++ local/darklight.js
@@ -3,4 +3,8 @@ window.onload = function() {
     darklight.addEventListener('click', function() {
         document.body.classList.toggle('darkTheme');
     })
+    const darklightagain = document.querySelector('.secondaryToggleDarkTheme');
+    darklightagain.addEventListener('click', function() {
+        document.body.classList.toggle('darkTheme');
+    })
 }
