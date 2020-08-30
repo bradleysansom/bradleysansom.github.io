8ce5c43e623b8c1e7e60fc8b97ca241821e33cb5
bradleysansom <bjsansom@outlook.com>
2020-08-30 16:15:34 +0100

    Update postcodes.js

--- local/postcodes.js
+++ local/postcodes.js
@@ -1,39 +1,16 @@
+function changePage() {
+    var dashboardStylesheet = document.createElement('link rel="stylesheet" href="style/dashboard.css"');
+    document.body.appendChild(dashboardStylesheet);
+}
+
+
+
+
+
+
 function savePostcode(userPostcode) {
     var sendPostcode = document.getElementById('sendPostcode')[0].value;
     sendPostcode = sendPostcode.replace(/\s+/g, '');
     sessionStorage.setItem("userPostcode", sendPostcode);
+    changePage;
 }
-
-/*
-function sendToUrl() {
-    var appendToUrl = "http://bradleysansom.github.io/local/dashboard.html" + document.getElementsByName("userPostcode")[0].value;
-    var sendPostcode = document.getElementById('sendPostcode')[0];
-    sendPostcode.action = appendToUrl;
-    /* var url = window.location.href;
-        var postcodeCurrent = url.split('=')[0]; 
-    window.location.href = "http://bradleysansom.github.io/local/dashboard.html";
-    document.getElementById('postalCode').innerHTML = postcodeCurrent;
-}
-
-*/
-
-/*
-var userPostcode = document.getElementById("userPostcode").value;
-fetch('https://api.postcodes.io/postcodes/' + userPostcode)
-    .then(response => {
-        if (response.ok) {
-            return response.json()
-        } else {
-            return Promise.reject({
-                status: response.status,
-                statusText: response.statusText
-            })
-        }
-    })
-    .then(data => {
-        // Here's a list of repos!
-        console.log(data)
-    });
-
-
-    */
