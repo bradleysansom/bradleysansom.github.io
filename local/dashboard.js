0cad03dafbe8c6e7d3a7aa1e2fefd9ebc19fd20c
bradleysansom <bjsansom@outlook.com>
2020-09-02 02:14:47 +0100

    brian

--- local/dashboard.js
+++ local/dashboard.js
@@ -3,14 +3,25 @@ function dashboard() {
     dashboardDarklight();
     loadPostcodeData();
 }
-var posts;
-var adminDistrict;
+
 const loadPostcodeData = async() => {
     var cleanPostcode = localStorage.getItem("cleanPostcode");
     const response = await fetch('https://api.postcodes.io/postcodes/' + cleanPostcode);
     var posts = await response.json();
-    // console.log(posts);
-    // console.log(posts.result.admin_district.value);
-    var adminDistrict = posts.result.admin_district;
-    localStorage.setItem("adminDistrict", adminDistrict.value);
+    console.log(posts);
+    localStorage.setItem("posts", JSON.stringify(posts));
+}
+
+function storePostcodeData() {
+    var stringPosts = localStorage.getItem("posts");
+    //console.log('stringPosts', stringPosts)
+    var arrayPosts = JSON.parse(stringPosts);
+    // console.log('arrayPosts', arrayPosts);
+    // console.log(arrayPosts.result);
+    var resultsPosts = arrayPosts.result;
+    console.log(resultsPosts);
+    console.log('admin_county', resultsPosts.admin_county)
+    let adminCounty = resultsPosts.admin_county;
+    // let { admin_county, admin_district, admin_ward, ccg, codes, country, eastings, european_electoral_region, incode, latitude, longitude, lsoa, msoa, nhs_ha, northings, nuts, outcode, parish, parliamentary_constituency, postcode, primary_care_trust, quality, region } = resultsPosts;
+    console.log(admin_county);
 }
