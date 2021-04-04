9f463b1255a521d8f76247cc4c0e3ca507ff8cea
bradleysansom <bjsansom@outlook.com>
2021-04-04 13:53:53 +0100

    reverting to when it worked

--- local/dashboard.js
+++ local/dashboard.js
@@ -1,34 +1,19 @@
-//function dashboard() {
-//  postcodeHeading();
-//    dashboardDarklight();
-//
-//}
+function dashboard() {
+    postcodeHeading();
+    dashboardDarklight();
 
-async function loadPostcodeData() {
+}
+
+const loadPostcodeData = async() => {
     var cleanPostcode = localStorage.getItem("cleanPostcode");
-
     const response = await fetch('https://api.postcodes.io/postcodes/' + cleanPostcode);
     var posts = await response.json();
-
     console.log(posts);
-
+    localStorage.setItem("posts", JSON.stringify(posts));
 }
 
-function postcodeHandler() {
-    var cleanPostcode = localStorage.getItem("cleanPostcode");
-    var cleanPostcodeRequestUrl = 'https://api.postcodes.io/postcodes/' + cleanPostcode;
-    fetch(cleanPostcodeRequestUrl)
-        .then(response => response.json())
-        .then(data => {
-            var posts = data;
-            console.log(posts);
-            localStorage.setItem("posts", JSON.stringify(posts));
-        })
-        .catch(error => console.log('error is', error));
-}
 
-async function storePostcodeData() {
-    console.log("storing started");
+function storePostcodeData() {
     var stringPosts = localStorage.getItem("posts");
     var arrayPosts = JSON.parse(stringPosts);
     var resultsPosts = arrayPosts.result;
@@ -75,7 +60,6 @@ async function storePostcodeData() {
     localStorage.setItem("primary_care_trust", primary_care_trust);
     var region = resultsPosts.region;
     localStorage.setItem("region", region);
-    console.log("storing finished");
     // Set headings
     document.getElementById("postalCode").innerHTML = postcode;
     var welcomeHome = 'Welcome to ' + admin_district;
@@ -127,4 +111,212 @@ async function storePostcodeData() {
     } else {
         document.getElementById("postcard").style.backgroundImage = "url(images/housing.jpg)";
     }
+}
+
+const plateDemocracy = async() => {
+    var parliamentary_constituency = localStorage.getItem("parliamentary_constituency");
+    var parliamentary_constituencyPluses = parliamentary_constituency.split(' ').join('+');
+    // Fetch information about the MP
+    var parltURL = 'https://data.parliament.uk/membersdataplatform/services/mnis/members/query/constituency=' + parliamentary_constituencyPluses + '/';
+    const xmlFetch = await fetch(parltURL)
+    const xmlText = await xmlFetch.text()
+    const xml = await (new window.DOMParser()).parseFromString(xmlText, "text/xml")
+        // Save information about the MP
+    var memberName = xml.getElementsByTagName("DisplayAs")[0].childNodes[0].nodeValue;
+    localStorage.setItem("memberName", memberName);
+    var memberFullName = xml.getElementsByTagName("FullTitle")[0].childNodes[0].nodeValue;
+    localStorage.setItem("memberFullName", memberFullName);
+    var memberParty = xml.getElementsByTagName("Party")[0].childNodes[0].nodeValue;
+    localStorage.setItem("memberParty", memberParty);
+    var memberGender = xml.getElementsByTagName("Gender")[0].childNodes[0].nodeValue;
+    localStorage.setItem("memberGender", memberGender);
+    // Save pronouns of the MP
+    if (memberGender === "M") {
+        var memberPronoun = "He";
+        var memberPronounPlural = "He's";
+        var memberPronounObjective = "Him";
+        var memberPronounPosessive = "His";
+        var memberPronounLowercase = "he";
+        var memberPronounPluralLowercase = "he's";
+        var memberPronounObjectiveLowercase = "him";
+        var memberPronounPosessiveLowercase = "his";
+    } else if (memberGender === "F") {
+        var memberPronoun = "She";
+        var memberPronounPlural = "She's";
+        var memberPronounObjective = "Her";
+        var memberPronounPosessive = "Her";
+        var memberPronounLowercase = "she";
+        var memberPronounPluralLowercase = "she's";
+        var memberPronounObjectiveLowercase = "her";
+        var memberPronounPosessiveLowercase = "her";
+    } else {
+        var memberPronoun = "They";
+        var memberPronounPlural = "They're";
+        var memberPronounObjective = "Them";
+        var memberPronounPosessive = "Their";
+        var memberPronounLowercase = "they";
+        var memberPronounPluralLowercase = "they're";
+        var memberPronounObjectiveLowercase = "them";
+        var memberPronounPosessiveLowercase = "their";
+    }
+    var memberConstituency = xml.getElementsByTagName("MemberFrom")[0].childNodes[0].nodeValue;
+    localStorage.setItem("memberConstituency", memberConstituency);
+    var memberStartDate = xml.getElementsByTagName("HouseStartDate")[0].childNodes[0].nodeValue;
+    localStorage.setItem("memberStartDate", memberStartDate);
+    var memberStartYear = memberStartDate.substr(0, 4);
+    localStorage.setItem("memberStartYear", memberStartYear);
+    var memberId = xml.getElementsByTagName("Member")[0].getAttribute('Member_Id');
+    localStorage.setItem("memberId", memberId);
+    var memberPartyId = xml.getElementsByTagName("Party")[0].getAttribute('Id');
+    localStorage.setItem("memberPartyId", memberPartyId);
+    var memberPostcode = localStorage.getItem("postcode");
+    var theyWorkForYouUrl = 'https://www.theyworkforyou.com/mp/?c=' + parliamentary_constituencyPluses;
+    var writeToThemUrl = 'https://www.writetothem.com/?a=WMC&pc=' + localStorage.getItem("cleanPostcode");
+
+    document.getElementById("memberPostcode").innerHTML = memberPostcode;
+    document.getElementById("constituency").innerHTML = memberConstituency;
+    document.getElementById("memberStartYear").innerHTML = memberStartYear;
+    document.getElementById("memberImage").style.backgroundImage =
+        'url(https://data.parliament.uk/membersdataplatform/services/images/MemberPhoto/' + memberId + '/)';
+    document.getElementById("memberFullName").innerHTML = memberFullName;
+    document.getElementById("theyWorkForYouLink").innerHTML = 'See ' + memberPronounPosessiveLowercase + ' voting record and speeches';
+    document.getElementById("theyWorkForYouLink").href = theyWorkForYouUrl;
+    document.getElementById("writeToThemLink").innerHTML = 'Send ' + memberPronounObjectiveLowercase + ' a message';
+    document.getElementById("writeToThemLink").href = writeToThemUrl;
+    if (memberParty === "Speaker") {
+        document.getElementById("memberPartyContainer").innerHTML = memberPronoun + " is the Speaker of the House of Commons. Therefore " + memberPronounLowercase + " doesn't vote or participate in debates, and remains neutral on national issues.";
+    } else if (memberParty === "Independent") {
+        if (memberFullName === "Rt Hon Dr Julian Lewis MP") {
+            document.getElementById("memberPartyContainer").innerHTML = "He was formerly a Conservative MP, but is now an Independent MP.";
+        } else if (memberFullName === "Jonathan Edwards MP") {
+            document.getElementById("memberPartyContainer").innerHTML = "He was formerly a Plaid Cymru MP, but is now an Independent MP.";
+        } else {
+            document.getElementById("memberPartyContainer").innerHTML = memberPronounPlural + " an Independent MP.";
+        }
+    } else if (memberParty === "Sinn Féin") {
+        document.getElementById("memberPartyContainer").innerHTML = memberPronounPlural + " an MP for Sinn Féin. They do not attend Parliament as they refuse to take their seats, believing Northern Ireland should be a part of a United Ireland. ";
+    } else if (memberParty === "Alliance") {
+        document.getElementById("memberPartyContainer").innerHTML = memberPronounPlural + " an " + memberParty + ' MP';
+    } else {
+        document.getElementById("memberPartyContainer").innerHTML = memberPronounPlural + " a " + memberParty + ' MP';
+    }
+
+    if (memberParty === "Conservative") {
+        document.getElementById("partyImage").style.backgroundImage = "url('logos/tory.png')";
+        document.getElementById("memberBadges").style.background = "#00bfff";
+    } else if (memberParty === "Labour") {
+        document.getElementById("partyImage").style.backgroundImage = "url('logos/lab.png')";
+        document.getElementById("memberBadges").style.background = "#e91639";
+    } else if (memberParty === "Liberal Democrat") {
+        document.getElementById("partyImage").style.backgroundImage = "url('logos/lib.png')";
+        document.getElementById("memberBadges").style.background = "#fcca36";
+    } else if (memberParty === "Green Party") {
+        document.getElementById("partyImage").style.backgroundImage = "url('logos/green.png')";
+        document.getElementById("memberBadges").style.background = "#7ec94f";
+    } else if (memberParty === "Alliance") {
+        document.getElementById("partyImage").style.backgroundImage = "url('logos/alliance.png')";
+        document.getElementById("memberBadges").style.background = "#ffcc33";
+    } else if (memberParty === "Democratic Unionist Party") {
+        document.getElementById("partyImage").style.backgroundImage = "url('logos/dup.png')";
+        document.getElementById("memberBadges").style.background = "#243c5c";
+    } else if (memberParty === "Plaid Cymru") {
+        document.getElementById("partyImage").style.backgroundImage = "url('logos/plaid.png')";
+        document.getElementById("memberBadges").style.background = "#ffd633";
+    } else if (memberParty === "Scottish National Party") {
+        document.getElementById("partyImage").style.backgroundImage = "url('logos/snp.png')";
+        document.getElementById("memberBadges").style.background = "#fff2cc";
+    } else if (memberParty === "Sinn Féin") {
+        document.getElementById("partyImage").style.backgroundImage = "url('logos/sinn.png')";
+        document.getElementById("memberBadges").style.background = "#00cc6d";
+    } else if (memberParty === "Social Democratic & Labour Party") {
+        document.getElementById("partyImage").style.backgroundImage = "url('logos/sdlp.png')";
+        document.getElementById("memberBadges").style.background = "#00b383";
+    } else if (memberParty === "Speaker") {
+        document.getElementById("partyImage").style.backgroundImage = "url('logos/speaker.png')";
+        document.getElementById("memberBadges").style.background = "#222222";
+    } else {
+        document.getElementById("partyImage").style.background = "#000000";
+        document.getElementById("memberBadges").style.background = "#222222";
+    }
+}
+const plateWeather = async() => {
+
+}
+
+const plateNhs = async() => {
+    var country = localStorage.getItem("country");
+    var healthServiceName;
+    if (country === "England") {
+        healthServiceName = "NHS";
+        document.getElementById("plateNhs").getElementsByClassName("plateTitle")[0].innerHTML = healthServiceName;
+        // var nhsApiKey = prompt("Insert NHS API key:");
+        var postcode = localStorage.getItem("cleanPostcode");
+        var nhsApiUrl = 'https://api.nhs.uk/service-search/search-postcode-or-place?api-version=1&search=' + postcode;
+        let data = {
+            "filter": "(OrganisationTypeID eq 'SCL') or (OrganisationTypeID eq 'CLI') or (OrganisationTypeID eq 'DEN') or (OrganisationTypeID eq 'GPB') or (OrganisationTypeID eq 'GPP') or (OrganisationTypeID eq 'HOS') or (OrganisationTypeID eq 'MIU') or (OrganisationTypeID eq 'OPT') or (OrganisationTypeID eq 'PHA') or (OrganisationTypeID eq 'UC')",
+            "top": 25,
+            "skip": 0,
+            "count": true
+        };
+        //  const response = await fetch(nhsApiUrl, {
+        //    headers: {
+        //      credentials: 'include',
+        //    "Content-Type": "application/json",
+        //          "subscription-key": "769d38008e5a4a22affd1ab0c440e476"
+        //    },
+        //  data: data
+        //  });
+        //   var posts = await response.json();
+        // console.log(posts);
+
+        // localStorage.setItem("posts", JSON.stringify(posts));
+    } else if (country === "Scotland") {
+        healthServiceName = "NHS";
+        document.getElementById("plateNhs").getElementsByClassName("plateTitle")[0].innerHTML = healthServiceName;
+        var nhsInfoUrl = 'https://www.nhsinform.scot/scotlands-service-directory#maincontent';
+        document.getElementById("nhsInfoLink").innerHTML = 'See your local health services on NHS Inform';
+        document.getElementById("nhsInfoLink").href = nhsInfoUrl;
+    } else if (country === "Wales") {
+        healthServiceName = "NHS";
+        document.getElementById("plateNhs").getElementsByClassName("plateTitle")[0].innerHTML = healthServiceName;
+        var postcode = localStorage.getItem("cleanPostcode");
+        var nhsInfoUrl = 'http://www.wales.nhs.uk/ourservices/directory/postcodesearch?pc=' + postcode + '&dentist=1&gp=1&optician=1&pharmacy=1&dist=2'
+        document.getElementById("nhsInfoLink").innerHTML = 'See your local health services on NHS Wales';
+        document.getElementById("nhsInfoLink").href = nhsInfoUrl;
+    } else if (country === "Northern Ireland") {
+        healthServiceName = "HSC";
+        document.getElementById("plateNhs").getElementsByClassName("plateTitle")[0].innerHTML = healthServiceName;
+
+        var nhsInfoUrl = 'http://www.hscboard.hscni.net/health-services-useful-information/';
+        document.getElementById("nhsInfoLink").innerHTML = 'See your local health services on HSCNI';
+        document.getElementById("nhsInfoLink").href = nhsInfoUrl;
+    } else {
+        document.getElementById("plateNhs").style.display = "none";
+    }
+}
+
+function plateMap() {
+    var latitude = localStorage.getItem("latitude");
+    var longitude = localStorage.getItem("longitude");
+
+    var map = L.map('plateMap').setView({ lon: longitude, lat: latitude }, 2);
+
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+        maxZoom: 18,
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+
+    L.control.scale().addTo(map);
+    map.setZoom(17);
+}
+
+function plateTakeaway() {
+    var postcode = localStorage.getItem("cleanPostcode");
+    var justEatUrl = "https://www.just-eat.co.uk/area/" + postcode;
+    document.getElementById("justEatLink").href = justEatUrl;
+    var foodHubUrl = "https://foodhub.co.uk/list/" + postcode;
+    document.getElementById("foodHubLink").href = foodHubUrl;
 }
