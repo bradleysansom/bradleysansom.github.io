fb413a67be42b956b9309adb1fbf9b3df2b349ef
bradleysansom <bjsansom@outlook.com>
2021-05-02 00:51:36 +0100

    Update ie.js

--- local/ie.js
+++ local/ie.js
@@ -1,3 +1,7 @@
 if (window.document.documentMode) {
-    alert('Sorry!\nThis website does not work in your browser.\nPlease use another browser, such as Google Chrome or Firefox, to enjoy My Local Area.\nInstalling a new browser will also allow you to use new features on other websites.\n\nOnce again, My Local Area apologises for any inconvenience caused.')
+    if (confirm('Sorry!\nThis website does not work in your browser.\nPlease use another browser, such as Google Chrome or Firefox, to enjoy My Local Area.\nInstalling a new browser will also allow you to use new features on other websites.\n\nOnce again, My Local Area apologises for any inconvenience caused.')) {
+        document.getElementsByTagName('html')[0].style.display = "none";
+    } else {
+        document.getElementsByTagName('html')[0].style.display = "none";
+    };
 }
