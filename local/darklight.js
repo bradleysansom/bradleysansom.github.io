97b468496affeaf497a084e99c30c32a840fb6bb
bradleysansom <bjsansom@outlook.com>
2021-05-03 23:36:31 +0100

    colour scheme

--- local/darklight.js
+++ local/darklight.js
@@ -7,7 +7,23 @@ window.onload = function() {
     darklightagain.addEventListener('click', function() {
         document.body.classList.toggle('darkTheme');
     })
+
+
 }
+
+function checkTheme(preference) {
+    if (preference) {
+        console.log('prefers dark theme');
+        document.body.classList.toggle('lightTheme');
+        document.getElementsByClassName('toggleDarkTheme')[0].checked = true;
+    } else {
+        console.log('prefers light theme');
+        document.body.classList.toggle('darkTheme');
+        document.getElementsByClassName('toggleDarkTheme')[0].checked = false;
+    };
+};
+var preference = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
 
 function dashboardDarklight() {
     const darklight = document.querySelector('.toggleDarkTheme');
