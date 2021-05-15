198260e4a7c44d02eeeb711480ddb6112b88bb49
bradleysansom <bjsansom@outlook.com>
2021-05-15 13:35:41 +0100

    json array

--- local/plateCommunity.js
+++ local/plateCommunity.js
@@ -4,11 +4,24 @@ function plateCommunity() {
         .then(data => {
 
             console.log(data);
+            var stringOfCommunity = JSON.stringify(data);
+            console.log(stringOfCommunity);
+            var arrayOfCommunity = JSON.parse(stringOfCommunity);
+            console.log(arrayOfCommunity);
+            localStorage.setItem('arrayOfCommunity', arrayOfCommunity);
+            var groups = arrayOfCommunity.groups;
+            var services = arrayOfCommunity.services;
+            console.log(services);
+
+            // Groups
 
 
 
+            // Services
             var applicableOutcode = localStorage.getItem('outcode');
             console.log("Applicable outcode: " + applicableOutcode);
+            var applicableServices = services.filter(records => records.areas === applicableOutcode);
+            console.log(applicableServices);
 
 
 
