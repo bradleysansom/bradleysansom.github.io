6a78efb79c44b8fb33fef1ad52ea6e1076ae02a7
bradleysansom <bjsansom@outlook.com>
2021-05-15 13:07:46 +0100

    community groups JSON

--- local/plateCommunity.js
+++ local/plateCommunity.js
@@ -1,33 +1,18 @@
 function plateCommunity() {
-    fetch('https://bradleysansom.github.io/local/groups/groups.xml')
-        .then(response => response.text())
+    fetch('https://bradleysansom.github.io/local/groups/groups.json')
+        .then(response => response.json())
         .then(data => {
 
             console.log(data);
-            var parser = new DOMParser();
-            var communityOrgs = parser.parseFromString(data, "text/xml");
-            var community = (communityOrgs.getElementsByTagName('community'));
+
 
 
             var applicableOutcode = localStorage.getItem('outcode');
-            var outcodeQuery = "<outcode>" + applicableOutcode;
-
-
-            function communityGroup() {
-
-            }
-
-            function communityService() {
-                var services = communityOrgs.getElementsByTagName('service');
-                console.log(services);
-                var outcodes = communityOrgs.getElementsByTagName('service')[0].getElementsByTagName('areas')[0];
-                console.log(outcodes);
-                var iOutcodes;
+            console.log("Applicable outcode: " + applicableOutcode);
 
 
 
-            }
-            communityService();
+
 
         });
 
