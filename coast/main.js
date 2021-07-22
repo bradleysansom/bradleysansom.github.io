c8395418eef17ec5f58177e467adaec8b1a8db91
bradleysansom <19352051+bradleysansom@users.noreply.github.com>
2021-07-22 03:11:48 +0100

    los restaurantes

--- coast/main.js
+++ coast/main.js
@@ -1,5 +1,6 @@
 var options = {
-    valueNames: ['type', 'name', 'locality', 'postcode']
+    valueNames: ['type', 'name', 'locality', 'postcode', 'altname']
 };
 
 var userList = new List('places', options);
+
