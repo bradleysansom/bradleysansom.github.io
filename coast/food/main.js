ebbbe77a4391e031f26473472ea31b3f947d073c
bradleysansom <19352051+bradleysansom@users.noreply.github.com>
2021-07-27 21:21:26 +0100

    places to visit

--- coast/food/main.js
+++ coast/food/main.js
@@ -4,3 +4,11 @@ var options = {
 
 var userList = new List('places', options);
 
+var url = window.location.href;
+var arr = url.split('?')
+var desire = arr[1];
+console.log(desire);
+if (desire != undefined) {
+    document.querySelector("#searchbar").value = desire;
+    userList.search(desire);
+}
