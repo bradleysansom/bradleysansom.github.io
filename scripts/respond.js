8648a91bdca8b222d536b24f1a270bd3e4bf257c
Bradley Sansom <19352051+bradleysansom@users.noreply.github.com>
2023-01-24 01:12:09 +0000

    add share links

--- scripts/respond.js
+++ scripts/respond.js
@@ -1,9 +1,23 @@
 var respondViaTwitter = document.getElementById("respondViaTwitter");
 respondViaTwitter.href = "https://twitter.com/intent/tweet?text=" + window.location.href;
 
+var respondViaReddit = document.getElementById("respondViaReddit");
+respondViaReddit.href = "https://www.reddit.com/submit?url=" + window.location.href;
+
 var currentUrl = document.getElementById("urlGoesHere");
 currentUrl.textContent = window.location.href;
 
 var copyUrlButtonIcon = document.getElementById("copyUrlButtonIcon");
 var copiedUrl = copyUrlButtonIcon.classList;
+
+function respondViaMastodon() {
+
+    domain = prompt("Enter your Mastodon server URL", "mastodon.social");
+
+    if (domain == "" || domain == null) {
+        return;
+    }
+    url = "https://" + domain + "/share?text=" + window.location.href;
+    window.open(url, '_blank');
+}
 
