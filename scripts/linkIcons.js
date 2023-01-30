a029c7ce2a1acf3bbe3a590beca299c67c3a2580
Bradley Sansom <19352051+bradleysansom@users.noreply.github.com>
2023-01-30 23:09:50 +0000

    link icons

--- scripts/linkIcons.js
+++ scripts/linkIcons.js
@@ -1,17 +1,18 @@
-document.querySelectorAll("a.iconLink").forEach(function (element) {
-    var fontSize = getComputedStyle(element).getPropertyValue("font-size").replace("px", "");
-    console.log(fontSize);
-    var address = element.href.split("/")[2];
-    console.log(address);
-    var faviconUrl = "https://icons.duckduckgo.com/ip3/" + address + ".ico";
-    console.log(faviconUrl);
-    var icon = document.createElement("img");
-    icon.src = faviconUrl;
-    icon.height = fontSize;
-    icon.width = fontSize;
-    var linkText = document.createRange().createContextualFragment(element.innerHTML);
+function renderLinks() {
+    document.querySelectorAll("a.iconLink").forEach(function (element) {
+        var fontSize = getComputedStyle(element).getPropertyValue("font-size").replace("px", "");
+        console.log(fontSize);
+        var address = element.href.split("/")[2];
+        console.log(address);
+        var faviconUrl = "https://icons.duckduckgo.com/ip3/" + address + ".ico";
+        console.log(faviconUrl);
+        var icon = document.createElement("img");
+        icon.src = faviconUrl;
+        icon.height = fontSize;
+        icon.width = fontSize;
+        var linkText = document.createRange().createContextualFragment(element.innerHTML);
+        element.innerHTML = "";
+        element.append(icon, linkText);
 
-    element.innerHTML = "";
-    element.append(icon, linkText);
-
-});
+    });
+}
