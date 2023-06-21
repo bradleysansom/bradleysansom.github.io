a21909a8c58ac1512267a8ba67d46306a96cc6e1
Bradley Sansom <19352051+bradleysansom@users.noreply.github.com>
2023-06-21 21:33:20 +0100

    fix Terrors and other links, remove greta dead links

--- scripts/meltingframe.js
+++
@@ -1,6 +0,0 @@
-function meltingFrame() {
-    document.addEventListener('DOMContentLoaded', function () {
-        document.getElementById("greta").childNodes[1] = '<iframe class="carousel" width="100 % " height="250px" alt="Melting typeface" src="/melting/forgive.html"></iframe>'
-
-    }, false);
-}
